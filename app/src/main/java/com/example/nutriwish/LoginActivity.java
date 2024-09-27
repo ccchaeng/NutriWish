29416b08f0de4bc0f96bfaec868c68c0c7f30fb5
Chaeng <chaeng30415@naver.com>
2024-09-27 15:12:35 +0900

    로그인 버튼 클릭 시 메인화면으로 이동 추가

--- app/src/main/java/com/example/nutriwish/LoginActivity.java
+++ app/src/main/java/com/example/nutriwish/LoginActivity.java
@@ -13,14 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 public class LoginActivity extends AppCompatActivity {
 
     TextView sign;
+    TextView login;
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //로그인 버튼
+        login = findViewById(R.id.btn_login);
+
         //회원가입 버튼
         sign = findViewById(R.id.btn_signup);
+
+        //로그인 버튼 클릭시, 메인화면으로 이동
+        login.setOnClickListener(v -> {
+            Intent intent = new Intent(this, CalendarActivity.class);
+            startActivity(intent);
+        });
 
         //회원가입 버튼 클릭시, 회원가입 페이지로 이동
         sign.setOnClickListener(v -> {
