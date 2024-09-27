36fd1b9b422d906c90ba708666679199f464dd30
Chaeng <chaeng30415@naver.com>
2024-09-27 17:55:08 +0900

    하단네비게이션뷰 사용을 위한 정의 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.meterial:material:1.4.0")
+    implementation("com.android.support:design:29.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
