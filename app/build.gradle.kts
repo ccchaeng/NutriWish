ef484b95871499eb004b179d013142f7d8c8bd2b
rbgud7957 <r67890@naver.com>
2024-10-30 20:08:08 +0900

    데이터베이스 연결까지

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
 
 dependencies {
@@ -40,10 +41,12 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation("com.google.android.material:material:1.12.0")
     implementation ("androidx.fragment:fragment:1.5.7")
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))  // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))  // Firebase BoM
     implementation("com.google.firebase:firebase-analytics")              // Firebase Analytics
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-firestore")
 }
