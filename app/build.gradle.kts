c713a9017426d9f655d74ef949a517ee5ad2bbdf
Chaeng <chaeng30415@naver.com>
2024-11-05 18:20:06 +0900

    파이어베이스 연동 위한 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("com.google.gms.google-services")  // Google 서비스 플러그인
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -33,12 +35,21 @@ android {
 
 dependencies {
 
+    //파이어베이스 의존성 추가
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))  // Firebase BoM
+    implementation("com.google.firebase:firebase-analytics")              // Firebase Analytics
+    implementation("com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-firestore:25.1.1")
+    implementation("com.google.firebase:firebase-crashlytics")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("com.google.android.material:material:1.12.0")
     implementation ("androidx.fragment:fragment:1.5.7")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
