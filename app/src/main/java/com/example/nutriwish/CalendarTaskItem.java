67dfee9f79ad70416df9ec013d615263f986fd98
Chaeng <chaeng30415@naver.com>
2024-11-11 20:38:30 +0900

    일정 ID도 받아올 수 있게 수정함

--- app/src/main/java/com/example/nutriwish/CalendarTaskItem.java
+++ app/src/main/java/com/example/nutriwish/CalendarTaskItem.java
@@ -1,15 +1,24 @@
 package com.example.nutriwish;
 
 public class CalendarTaskItem {
+    private String id;
     private String taskName;
+    private String taskDate;
     private String taskTime;
     private String taskMemo;
 
-    public CalendarTaskItem(String taskName, String taskTime, String taskMemo) {
+    public CalendarTaskItem() {}
+
+    public CalendarTaskItem(String taskName, String taskDate, String taskTime, String taskMemo) {
         this.taskName = taskName;
+        this.taskDate = taskDate;
         this.taskTime = taskTime;
         this.taskMemo = taskMemo;
     }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
 
     public String getTaskName() {
         return taskName;
@@ -19,9 +28,11 @@ public class CalendarTaskItem {
         this.taskName = taskName;
     }
 
-    public String getTaskTime() {
-        return taskTime;
-    }
+    public String getTaskDate() { return taskDate; }
+
+    public void setTaskDate(String taskDate) { this.taskDate = taskDate; }
+
+    public String getTaskTime() { return taskTime; }
 
     public void setTaskTime(String taskTime) {
         this.taskTime = taskTime;
